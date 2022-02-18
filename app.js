084190a2434747a5931268eb33afd9ea8e01ef1f
“chekir <walid.chekir@esprit.tn>
2022-02-18 18:18:10 +0100

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express");
 
-const app = express();
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const logger = require("morgan");
+const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json({ type: 'application/json' }))
 
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
